ab60420c90eadad707a1e0615c0e31c13869d649
Jörn <s0542818@htw-berlin.de>
2015-01-20 12:11:52 -0800

    hier noch die sachen von schwotzer von letzter Uebung eingepfelgt

--- src/htw/shark/nowdiscover/Shop.java
+++ src/htw/shark/nowdiscover/Shop.java
@@ -1,19 +1,35 @@
 package htw.shark.nowdiscover;
 
 import htw.shark.nowdiscover.userutils.*;
+import net.sharkfw.knowledgeBase.SharkKBException;
 import net.sharkfw.system.*;
 
 public interface Shop {
-	public Category createCategory(String name, String url)
-			throws shopException, SharkException;
 
-	public Category createCategory(String name, String... url)
-			throws shopException, SharkException;
+    public Category createCategory(String name, String url)
+            throws shopException, SharkException;
 
-	public Product createProduct(String name);
+    public Category createCategory(String name, String... url)
+            throws shopException, SharkException;
 
-	public User createUser(String name, String url);
-	// remove..
-	// get..
+    public Product createProduct(String name);
+
+    public User createUser(String name, String url);
+
+    public void createProfile(User user);
+    
+    public Iterable<Profile> getProfiles() throws SharkKBException;
+
+    public Profile getProfile();
+
+    public void removeProfile(Profile profile);
+
+    public void createInterest(ShopConcept shopConcept, Profile profile,
+            String property, String value);
+
+    public void removeInterest(Interest interest);
+
+    public Interest getInterest();
 
 }
+
--- src/htw/shark/nowdiscover/userutils/Interest.java
+++ src/htw/shark/nowdiscover/userutils/Interest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package htw.shark.nowdiscover.userutils;
 
 import net.sharkfw.knowledgeBase.*;
@@ -14,111 +13,19 @@ import net.sharkfw.knowledgeBase.inmemory.*;
  * @author Jörn
  */
 public class Interest implements Interest_Interface {
-	InMemoSharkKB kb = new InMemoSharkKB();
 
-	private SemanticTag topic;
-	private PeerSemanticTag user;
-	private String name;
-	private String property;
-	private String value;
+    ContextPoint cp;
 
-	public Interest() {
-	}
+    public Interest(SemanticTag topic, PeerSemanticTag user, String value) throws SharkKBException {
+        //Shopengine= getkb
+        SharkKB kb = null;
+        cp = kb.getContextPoint(kb.createContextCoordinates(topic, user, null, null, null, null, SharkCS.DIRECTION_INOUT));
+    }
 
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public void setUrls(String[] urls) throws SharkKBException {
-		for (String url : urls) {
-			topic.addSI(url);
-		}
-	}
-
-	@Override
-	public void setProperty_Value(String property, String value) {
-		this.property = property;
-		this.value = value;
-		topic.setProperty(property, value);
-	}
-
-	@Override
-	public void setUsername(String name) {
-		user.setName(name);
-	}
-
-	@Override
-	public void setUserinfomation(String[] info) throws SharkKBException {
-		for (String infos : info) {
-			user.addSI(infos);
-		}
-	}
-
-	@Override
-	public String getUsername() {
-		return user.getName();
-	}
-
-	@Override
-	public String[] getUserinformation() {
-		return user.getSI();
-	}
-
-	@Override
-	public String getName() {
-		return topic.getName();
-	}
-
-	@Override
-	public String[] getUrls() {
-		return topic.getSI();
-	}
-
-	@Override
-	public String getProperty() {
-		return property;
-	}
-
-	@Override
-	public String getValue() {
-		return value;
-	}
-
-	@Override
-	public void remove_userinformation() throws SharkKBException {
-		for (String userinfo : getUserinformation()) {
-			user.removeSI(userinfo);
-		}
-	}
-
-	@Override
-	public void removeUrls() throws SharkKBException {
-		for (String url : getUrls()) {
-			topic.removeSI(url);
-		}
-	}
-
-	@Override
-	public void removeProperty_Value() {
-		topic.setProperty(name, null);
-		name = null;
-		value = null;
-
-	}
-
-	// Prototype
-	public void createInterest() throws SharkKBException {
-		ContextCoordinates cc = kb.createContextCoordinates(topic, user, user,
-				null, null, null, SharkCS.DIRECTION_NOTHING);
-		kb.createContextPoint(cc);
-
-	}
-
-	@Override
-	public Interest getInterest() {
-		return Interest.this;
-	}
-
+    public Interest(ContextPoint cp){
+    this.cp = cp;
+    
+    }
+    
+    
 }
